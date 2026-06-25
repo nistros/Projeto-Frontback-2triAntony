fbe2040fb2dfa367ce046bccf2a1145eaa4fb36f
nistros <antonykloss7@gmail.com>
2026-06-25 18:34:39 -0300

    Terminei a parte dois de html

---
+++ Lição 1/nome.js
@@ -0,0 +1,7 @@
+console.log("Antony Eduardo");
+console.log("O nome do projeto é: Projeto quadro de avisos");
+console.log("mensagem 1");
+console.log("mensagem 2");
+console.log("mensagem 3");
+console.log("Teste 1");
+console.log("Teste 2");
