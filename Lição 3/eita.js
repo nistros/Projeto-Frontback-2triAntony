5341ca92f0429bb73ff2a9ca0c351aed7cb5c15a
nistros <antonykloss7@gmail.com>
2026-06-25 23:24:07 -0300

    terminei!

--- Lição 3/eita.js
+++ Lição 3/eita.js
@@ -1,15 +1,52 @@
 const express = require('express');
 const app = express();
 
-const avisos = [
-  'Reunião de responsáveis',
-  'Prova de Matemática',
-  'Entrega de trabalhos'
+const usuarios = [
+  'Roberto Carlos',
+  'Roberto baggio',
+  'Eita gulosinho'
 ];
 
-app.get('/avisos', (req, res) => {
-  res.json(avisos);
+app.get('/usuarios', (req, res) => {
+  res.json(usuarios);
 });
+
+const tarefas = [
+  'Lavar louça',
+  'Limpar chão',
+  'Comprar carne'
+];
+
+app.get('/tarefas', (req, res) => {
+  res.json(tarefas);
+});
+
+
+const reservas = [
+  'Chris mesa 4 às 20',
+  'Charles mesa 9 às 23',
+  'Carlos mesa 12 às 22'
+];
+
+app.get('/reservas', (req, res) => {
+  res.json(reservas);
+});
+
+app.get('/status', (req, res) => {
+  res.send('Sistema ativo');
+});
+
+
+app.get('/turmas', (req, res) => {
+  res.send([
+    'Turma 1',
+    'Turma 2',
+    'Turma 3'
+  ]);
+});
+
+
+
 
 app.listen(3000, () => {
   console.log('Servidor rodando');
